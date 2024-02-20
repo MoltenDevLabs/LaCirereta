f08db827f392d63d56f4a669cb1065cf5e98cd46
Pol Cadavid <polkadavid@gmail.com>
2024-02-20 16:15:25 +0100

    upgrades

--- pages/pastissos.vue
+++ pages/pastissos.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="$device.isMobileOrTablet">
+    <!-- <div v-if="$device.isMobileOrTablet">
       <div class="flex flex-col">
         <img :src="cakeProduct.img" alt="Cake Image" class="rounded-md" />
         <h2 class="mt-0 mb-2">
@@ -30,10 +30,10 @@
             <select
               id="gustos"
               name="gustos"
-              v-model="selectedGust"
+              v-model="selectedCoc"
               class="select-product"
             >
-              <option v-for="option in cakeOptions" :value="option">
+              <option v-for="option in cocOptions" :value="option">
                 {{ option }}
               </option>
             </select>
@@ -43,7 +43,7 @@
             <select
               id="decoracio"
               name="decoracio"
-              v-model="selectedDecoration"
+              v-model="selectedFarcit"
               class="select-product"
             >
               <option value="Sense imatges">Sense imatges</option>
@@ -76,92 +76,102 @@
           </a>
         </div>
       </div>
-    </div>
-    <div v-if="$device.isDesktop" class="flex gap-8">
-      <h4 class="section-title">Tasta un pastís deliciós</h4>
-      <p>
-        Els meus pastissos personalitzats són la meva passió i el meu segell
-        distintiu. Els faig amb tota la cura i el detall que es mereixen, segons
-        els gustos i les preferències dels meus clients. Puc fer pastissos de
-        qualsevol ingredient, decoració o temàtica que vulguis. Només has de
-        dir-me quina és la teva idea i jo la faré realitat.
-      </p>
-      <div class="w-1/2 h-auto object-contain flex items-start not-prose">
-        <img
-          :src="cakeProduct.img"
-          alt="Cake Image"
-          class="rounded-md my-4 not-prose"
-        />
-      </div>
-      <div class="w-1/2">
-        <h4 class="section-title">
-          {{ cakeProduct.title }}
-        </h4>
-        <p class="my-0">{{ cakeProduct.description }}</p>
+    </div> -->
+    <div v-if="$device.isDesktop" class="flex flex-col gap-4">
+      <div>
+        <h4 class="section-title">Tasta un pastís deliciós</h4>
+        <p>
+          Els meus pastissos personalitzats són la meva passió i el meu segell
+          distintiu. Els faig amb tota la cura i el detall que es mereixen,
+          segons els gustos i les preferències dels meus clients. Puc fer
+          pastissos de qualsevol ingredient, decoració o temàtica que vulguis.
+          Només has de dir-me quina és la teva idea i jo la faré realitat.
+        </p>
         <hr />
-        <form action="">
-          <div class="flex justify-between items-center mx-2">
-            <label for="mida">MIDA: </label>
-            <select
-              id="mida"
-              name="mida"
-              v-model="selectedSize"
-              class="select-product"
+      </div>
+      <div class="flex flex-row gap-8">
+        <div class="w-3/5 flex flex-col items-start not-prose">
+          <div class="h-auto object-contain">
+            <img
+              src="https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2024-02-09T13%3A23%3A50.827Z"
+              alt="Cake Image"
+              class="rounded-md my-4 not-prose"
+            />
+          </div>
+          <div class="flex gap-4 overflow-hidden">
+            <productCard
+              v-for="cake in featuredList"
+              :key="cake.id"
+              :cake="cake"
+              class=""
+            />
+          </div>
+        </div>
+        <div class="flex flex-col justify-center w-2/5">
+          <form action="">
+            <div class="flex justify-between items-center mx-2">
+              <label for="mida">MIDA: </label>
+              <select
+                id="mida"
+                name="mida"
+                v-model="selectedSize"
+                class="select-product"
+              >
+                <option value="Petit">Petit (8-15 porcions)</option>
+                <option value="Mitjà">Mitjà (20-25 porcions)</option>
+                <option value="Gran">Gran (25-35 porcions)</option>
+                <option value="Especial">Especial (35+ porcions)</option>
+              </select>
+            </div>
+            <div class="flex justify-between items-center mx-2">
+              <label for="gustos"> GUST COC: </label>
+              <select
+                id="gustos"
+                name="gustos"
+                v-model="selectedCoc"
+                class="select-product"
+              >
+                <option v-for="option in cocOptions" :value="option">
+                  {{ option }}
+                </option>
+              </select>
+            </div>
+            <div class="flex justify-between items-center mx-2">
+              <label for="decoracio">GUST FARCIT: </label>
+              <select
+                id="decoracio"
+                name="decoracio"
+                v-model="selectedFarcit"
+                class="select-product"
+              >
+                <option v-for="option in farcitOptions" :value="option">
+                  {{ option }}
+                </option>
+              </select>
+            </div>
+            <div class="flex justify-between items-start mx-2">
+              <label for="tematica" class="mt-4">TEMÀTICA: </label>
+              <textarea
+                id="tematica"
+                name="tematica"
+                v-model="selectedTheme"
+                cols="30"
+                rows="4"
+                class="px-2 my-4 w-2/3 resize-none border rounded-md outline-surface-900 border-surface-900 dark:border-surface-300 dark:bg-surface-800"
+                placeholder="Explica la temàtica del teu pastís."
+              ></textarea>
+            </div>
+          </form>
+          <div class="flex flex-col items-stretch my-4 mx-[12%]">
+            <a
+              :href="generateWhatsAppLink()"
+              class="btn my-8 no-underline"
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <option value="Petit">Petit (8-15 porcions)</option>
-              <option value="Mitjà">Mitjà (20-25 porcions)</option>
-              <option value="Gran">Gran (25-35 porcions)</option>
-            </select>
+              Contacta
+            </a>
           </div>
-          <div class="flex justify-between items-center mx-2">
-            <label for="gustos">GUST: </label>
-            <select
-              id="gustos"
-              name="gustos"
-              v-model="selectedGust"
-              class="select-product"
-            >
-              <option v-for="option in cakeOptions" :value="option">
-                {{ option }}
-              </option>
-            </select>
-          </div>
-          <div class="flex justify-between items-center mx-2">
-            <label for="decoracio">DECORACIÓ: </label>
-            <select
-              id="decoracio"
-              name="decoracio"
-              v-model="selectedDecoration"
-              class="select-product"
-            >
-              <option value="Sense imatges">Sense imatges</option>
-              <option value="Xocotransfer">Xocotransfer (+12€)</option>
-              <option value="Paper de sucre">Paper de sucre (+10€)</option>
-            </select>
-          </div>
-          <div class="flex justify-between items-start mx-2">
-            <label for="tematica" class="mt-4">TEMÀTICA: </label>
-            <textarea
-              id="tematica"
-              name="tematica"
-              v-model="selectedTheme"
-              cols="30"
-              rows="4"
-              class="px-2 my-4 w-2/3 resize-none border rounded-md outline-surface-900 border-surface-900 dark:border-surface-300 dark:bg-surface-800"
-              placeholder="Explica la temàtica del teu pastís."
-            ></textarea>
-          </div>
-        </form>
-        <div class="flex justify-between items-center mx-4">
-          <p class="text-xl lg:text-2xl">TOTAL: {{ totalPrice }}€</p>
-          <a
-            :href="generateWhatsAppLink()"
-            class="btn my-8 no-underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Contacta
-          </a>
         </div>
       </div>
     </div>
@@ -170,61 +180,27 @@
 
 <script setup>
 import { useCakeStore } from "@/stores/cakeStore";
-const route = useRoute();
 const cakeStore = useCakeStore();
-
-const cakes = [
-  ...JSON.parse(JSON.stringify(cakeStore.pastissosList)),
-  ...JSON.parse(JSON.stringify(cakeStore.browniesList)),
-  ...JSON.parse(JSON.stringify(cakeStore.cheesecakesList)),
-  ...JSON.parse(JSON.stringify(cakeStore.tematitzatsList)),
-];
-const data = cakes.find((c) => c.title === route.params.title);
-
-const cakeProduct = ref(data);
+const featuredList = cakeStore.featuredList;
 
 const selectedSize = ref("Petit");
-const selectedDecoration = ref("Sense imatges");
-const selectedGust = ref("Xocolata");
+const selectedFarcit = ref("Trufa");
+const selectedCoc = ref("Vainilla");
 const selectedTheme = ref("");
 
-let cakeOptions;
-if (cakeProduct.value.title.toLowerCase() === "red velvet") {
-  cakeOptions = ref(["Red Velvet"]);
-  selectedGust.value = "Red Velvet";
-} else if (cakeProduct.value.title.toLowerCase() === "carrot cake") {
-  cakeOptions = ref(["Carrot Cake"]);
-  selectedGust.value = "Carrot Cake";
-} else {
-  cakeOptions = ref(["Xocolata", "Crema Kinder", "Oreo", "Crema Mascarpone"]);
-}
+let cocOptions;
+cocOptions = ref(["Vainilla", "Xocolata"]);
 
-const totalPrice = computed(() => {
-  let sizePrice = 0;
-  if (selectedSize.value) {
-    sizePrice = cakeProduct.value.price[selectedSize.value];
-  }
-
-  let decorationPrice = 0;
-  if (selectedDecoration.value === "Xocotransfer") {
-    decorationPrice = 12;
-  } else if (selectedDecoration.value === "Paper de sucre") {
-    decorationPrice = 10;
-  }
-
-  return sizePrice + decorationPrice;
-});
+let farcitOptions;
+farcitOptions = ref(["Trufa", "Nata", "Kinder"]);
 
 const generateWhatsAppLink = () => {
-  const message = `Hola! M'interessa el ${
-    cakeProduct.value.title.charAt(0).toUpperCase() +
-    cakeProduct.value.title.slice(1)
-  }.`;
+  const message = `Hola! M'interessa el pastís següent:`;
 
   const selectedOptions = `
   \u{1F4CF} Mida: ${selectedSize.value},
-  \u{1F370} Gust: ${selectedGust.value},
-  \u{1FA84} Decoració: ${selectedDecoration.value},
+  \u{1F370} Gust coc: ${selectedCoc.value},
+  \u{1FA84} Gust farcit: ${selectedFarcit.value},
   \u{1F4DD} Temàtica: ${selectedTheme.value}`;
 
   const phoneNumber = "34699233456";
--- stores/cakeStore.js
+++ stores/cakeStore.js
@@ -2,33 +2,33 @@ export const useCakeStore = defineStore("cakeStore", () => {
   const featuredList = ref([
     {
       id: 1,
-      title: "cake 1",
+      title: "Unicorn",
       description: "cake 1 description",
-      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2023-11-06T21%3A49%3A51.835Z",
+      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/unicorn/unicorn-front.jpg?t=2024-02-09T14%3A30%3A44.226Z",
     },
     {
       id: 2,
-      title: "cake 2",
+      title: "Pinotxo",
       description: "cake 2 description",
-      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2023-11-06T21%3A49%3A51.835Z",
+      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/pinotxo/pinotxo-front.jpg?t=2024-02-09T14%3A31%3A16.953Z",
     },
     {
       id: 3,
-      title: "cake 3",
+      title: "Gegants",
       description: "cake 3 description",
-      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2023-11-06T21%3A49%3A51.835Z",
+      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/gegants/gegants-front.jpg?t=2024-02-09T14%3A31%3A37.888Z",
     },
     {
       id: 4,
-      title: "cake 4",
+      title: "Goku",
       description: "cake 4 description",
-      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2023-11-06T21%3A49%3A51.835Z",
+      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/goku/goku-front.jpeg?t=2024-02-09T14%3A32%3A37.624Z",
     },
     {
       id: 5,
-      title: "cake 5",
+      title: "Bluey",
       description: "cake 5 description",
-      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2023-11-06T21%3A49%3A51.835Z",
+      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/bluey/bluey-front.jpg?t=2024-02-09T14%3A33%3A01.420Z",
     },
     {
       id: 6,
@@ -44,59 +44,7 @@ export const useCakeStore = defineStore("cakeStore", () => {
     },
   ]);
 
-  const pastissosList = ref([
-    {
-      id: 101,
-      title: "pastis de xocolata",
-      description:
-        "Aixo es una prova pel pastis de xocolata amb una mica mes de text per omplir. D'aquesta manera es pot veure com quedara un text una mica més llarg on s'expliqui una mica de que va aquest pastis, lo bó que és i perquè l'has de tastar.",
-      price: {
-        Petit: 10.5,
-        Mitjà: 20.5,
-        Gran: 30.5,
-      },
-      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2023-11-06T21%3A49%3A51.835Z",
-    },
-    {
-      id: 102,
-      title: "pastis de maduixes",
-      description:
-        "Aixo es una prova pel pastis de maduixes amb una mica mes de text per omplir. D'aquesta manera es pot veure com quedara un text una mica més llarg on s'expliqui una mica de que va aquest pastis, lo bó que és i perquè l'has de tastar.",
-      price: {
-        Petit: 12.5,
-        Mitjà: 21.5,
-        Gran: 93.5,
-      },
-      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2023-11-06T21%3A49%3A51.835Z",
-    },
-    {
-      id: 103,
-      title: "red velvet",
-      description:
-        "Deliciós pastís vermell, farcit de crema mascarpone. En tallar-lo et sorpendrà l'intens color vermell del coc, i la crema mascarpone li dona una cremositat inigualable.",
-      price: {
-        Petit: 53.2,
-        Mitjà: 87.1,
-        Gran: 153.2,
-      },
-      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2023-11-06T21%3A49%3A51.835Z",
-    },
-    {
-      id: 104,
-      title: "carrot cake",
-      description:
-        "Deliciós pastís de pastanaga, el classic pastís de pastanaga, amb la millor recepta que es pot trobar.",
-      price: {
-        Petit: 53.2,
-        Mitjà: 87.1,
-        Gran: 153.2,
-      },
-      img: "https://lophahzsxonguopnjuyr.supabase.co/storage/v1/object/public/selecta-cakes/sample.jpg?t=2023-11-06T21%3A49%3A51.835Z",
-    },
-  ]);
-
   return {
     featuredList,
-    pastissosList,
   };
 });
